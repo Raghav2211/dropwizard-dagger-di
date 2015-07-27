6b1751ea9004cfe75faff3062bbce5d1b3b6aef4
neo <raghav@infoobjects.com>
2015-07-27 12:33:52 +0530

    Update
    Change method providesUserBean to provideUserBean and remove Userbean .class form @Module

--- src/main/java/org/example/config/DaggerModule.java
+++ src/main/java/org/example/config/DaggerModule.java
@@ -1,26 +1,15 @@
 package org.example.config;
 
-import dagger.Module;
-import dagger.Provides;
 import org.example.api.UserBean;
 import org.example.service.HelloWorldService;
 
-import javax.inject.Singleton;
+import dagger.Module;
+import dagger.Provides;
 
-@Module (
-        injects = {HelloWorldService.class, UserBean.class},
-        library = true
-        )
-
+@Module(injects = { HelloWorldService.class }, library = true)
 public class DaggerModule {
-    @Provides
-    @Singleton
-    HelloWorldService provideHelloService(){
-        return new HelloWorldService();
-    }
-
-    @Provides
-    UserBean providesUserBean(){
-        return new UserBean();
-    }
+	@Provides
+	UserBean provideUserBean() {
+		return new UserBean();
+	}
 }
